12a3762a518531f0108f458de92e5bd00610d0e3
huangjun <yhhj123@yeah.net>
2013-02-04 17:24:26 +0800

    new project

---
+++ myproject/myproject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  myproject
+//
+//  Created by hj on 13-2-4.
+//  Copyright (c) 2013年 huangjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
